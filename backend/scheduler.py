76b24226b5048ffb6035b1d17152b88b126691ce
Fedor Grab <fvgrab@gmail.com>
2020-04-04 00:41:54 -0400

    Turn on scheduler

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,13 +1,18 @@
-# import atexit
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from backend.data_mining import update_data
-# from backend.application.settings import BackendSettings
-#
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(
-#     func=update_data,
-#     trigger="interval",
-#     hours=BackendSettings.UPDATE_DATA_EVERY_X_HOUR
-# )
-# scheduler.start()
-# atexit.register(lambda: scheduler.shutdown())
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from backend.data_mining import update_data, update_total_data
+from backend.application.settings import BackendSettings
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(
+    func=update_data,
+    trigger="interval",
+    hours=BackendSettings.UPDATE_DATA_EVERY_X_HOUR
+)
+scheduler.add_job(
+    func=update_total_data,
+    trigger="interval",
+    hours=BackendSettings.UPDATE_DATA_EVERY_X_HOUR
+)
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
